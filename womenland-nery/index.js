505211528a3669e0cef45358f8b6986ceb98f8b0
Sayanery <121173735+sayanery@users.noreply.github.com>
2024-05-27 23:06:48 +0700

    Add files via upload

---
+++ womenland-nery/index.js
@@ -0,0 +1,34 @@
+let sky = document.getElementById("sky")
+let woman = document.getElementById("woman")
+let sea = document.getElementById("sea")
+let land = document.getElementById("land")
+let decor = document.getElementById("decor")
+let cloud = document.getElementById("cloud")
+let island = document.getElementById("island")
+let boat = document.getElementById("boat")
+let text = document.getElementById("text")
+let section = document.getElementById("content")
+
+const image = document.querySelector(".ukiyo");
+new Ukiyo(image);
+
+function parallaxSetup() {
+  let value = window.scrollY
+  if (value < window.innerWidth / 2) {
+    island.style.left = value * -2 + 'px'
+    boat.style.left = value + 'px'
+    text.style.transform = `translateY(${(value / 1.5)}px)`;
+  }
+  if (value < 400) {
+    woman.style.transform=`translateY(${(value / 2)}px)`;
+    land.style.transform = `translateY(${(value / 2)}px)`;
+    decor.style.transform = `translateY(${(value / 2)}px)`;
+    sea.style.transform = `translateY(${(value / 3)}px)`;
+    island.style.transform = `translateY(${(value / 3)}px)`;
+    boat.style.transform = `translateY(${(value / 3)}px)`;
+    section.style.top = Math.floor(land.offsetHeight + (value / 2)) + 'px'
+  }
+}
+
+window.addEventListener('scroll', parallaxSetup)
+window.addEventListener('resize', parallaxSetup)
